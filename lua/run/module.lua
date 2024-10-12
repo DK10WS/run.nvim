ed10fe3c503844777d1ba821fe9798dc91403ae4
Dhruv Kunzru <dhruvkunzru@Gmail.com>
2024-10-13 01:32:35 +0530

    testing run

--- lua/run/module.lua
+++ lua/run/module.lua
@@ -1,9 +1,23 @@
----@class CustomModule
 local M = {}
 
----@return string
-M.my_first_function = function(greeting)
-  return greeting
+M.run_code = function()
+  local ft = vim.bo.filetype
+  local cmd = ""
+
+  if ft == "python" then
+    cmd = "python " .. vim.fn.expand("%")
+  elseif ft == "cpp" then
+    cmd = "g++ " .. vim.fn.expand("%") .. " -o output && ./output"
+  elseif ft == "c" then
+    cmd = "gcc " .. vim.fn.expand("%") .. " -o output && ./output"
+  elseif ft == "lua" then
+    cmd = "lua " .. vim.fn.expand("%")
+  else
+    print("Filetype not supported!")
+    return
+  end
+
+  vim.cmd("!" .. cmd)
 end
 
 return M
--- plugin/run.lua
+++ plugin/run.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_create_user_command("MyFirstFunction", require("run").hello, {})
+vim.api.nvim_set_keymap("n", "<C-R>", ':lua require("run.module").run_code()<CR>', { noremap = true, silent = true })
